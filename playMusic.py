a1b2df727af584a6fe7b5568483e03cd1f54e10a
Ruthvik Peddawandla <ruthvik.ped94@gmail.com>
2015-03-29 21:46:02 -0700

    added wolfram

--- playMusic.py
+++ playMusic.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-import wolframalpha
+import wolframalpha from wolframalpha
 
 #parse all the data from the wit json output
 response = requests.get(sys.argv[1])
@@ -12,13 +12,35 @@ data = response.json()
 command = data["_text"]
 intent = data["intent"]
 
+#if intent is play (music)
+if intent=="play":
 #assuming the second word in the command is the name of the playlist
-if len(command) > 2:
-	playlist = command[1]
+	if len(command) > 2:
+		playlist = command[1]
 
-else :
-	print "Error. No playlist name given"
-	return
+	else :
+		print "Error. No playlist name given"
+		return
 
+elif intent=="search":
+	searchWolfram(command)
 
+def searchWolfram(query):
 
+	app_id = AX9HR8-JQYEK5WJL6
+
+	client = wolframalpha.Client(app_id)
+
+	resp = client.query(query)
+
+	if len(resp.pods) > 0:
+    	texts = ""
+    	pod = resp.pods[1]
+    	if pod.text:
+        	texts = pod.text
+    	else:
+        	texts = "I have no answer for that"
+ 
+   		print texts
+	else:
+    	print "Unkown search command"

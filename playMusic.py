a47909ee5020a66e40568da1c3c9f0c28000f0ce
Ruthvik Peddawandla <ruthvik.ped94@gmail.com>
2015-04-03 12:28:59 -0700

    modified MPDO class functions

--- playMusic.py
+++ playMusic.py
@@ -17,6 +17,7 @@ if intent=="play":
 #assuming the second word in the command is the name of the playlist
 	if len(command) > 2:
 		playlist = command[1]
+		#create MPD wrapper object
 		playControl = MPDO() 
 		playControl.control(playlist)
 
@@ -40,19 +41,20 @@ def searchWolfram(query):
 	client = wolframalpha.Client(app_id)
 	resp = client.query(query)
 
-	#if more than one response
-	if len(resp.pods) > 0:
-    		ans = ""
-    		pod = resp.pods[1]
-    	if pod.text:
-        	ans = pod.text
-    	else:
-        	ans = "No answer found"
+	ans = ""
+	#pods[0] is the title of the returned list of results
+	pod = resp.pods[1]
+	#if there's a response
+	if pod.text:
+    	ans = pod.text
+	else:
+    	ans = "No answer found"
  
    	print ans
 
 class MPDO(object):
     def __init__(self):
+    	#we will need server and port names/numbers to connect to local MPD client
         self.server = "localhost"
         self.port = 6600
 
@@ -66,12 +68,12 @@ class MPDO(object):
         self.playlists = [x["playlist"] for x in self.client.listplaylists()]
 
         #define function that takes in playlist name and plays
-        def play(self, playlist_name):
-	        if playlist_name:
-	            self.client.clear()
-	            self.client.load(playlist_name)
-
-	        self.client.play()
+    def play(self, playlist_name):
+    	#empty current play queue
+        self.client.clear()
+        #load new playlist and then play - these are mpdclient commands
+        self.client.load(playlist_name)
+        self.client.play()
 
 
 
